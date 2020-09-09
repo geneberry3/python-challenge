06a21c5c266c818de57d451a5ad5a087a76c7c52
geneberry3 <69098033+geneberry3@users.noreply.github.com>
2020-09-08 23:07:34 -0700

    Add files via upload
    I am getting there... but to no avail

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 #import module to read csv
 import csv
 total_month = 0
-total_profit= 0
+profit = 0
 
 # loss=0
 #source path for csv and define
@@ -13,42 +13,16 @@ with open(budget_csv) as csvfile:
     csv_header = next(csv_reader)
     for row in csv_reader:
         total_month += 1
-profit = int(row[1])        
-profit=0    
-net_profit = budget_csv['profit'].value_counts()
+    def total_profit(csv_reader):
+        profit = int(csv_reader[1])
+        for profit in csv_reader:
+            total_profit += profit
 
-# if profit > 0:
-    # total_profit= total_profit + profit
-# print(f'{total_month}')
-# print(f'{total_profit}')     
-
-
-
-
-# # #read file using CSV module
-# # with open(csvpath) as csvfile:
-
-
-# #     #read header row first
-# #     csv_header = next(csvreader)
-# #     print(f"CSV Header: {csv_header}")
-
-# #     #read each row after header
-# #     for row in csvreader:
-# #         print(row)
-
-# with open(budget_csv, 'r') as csvfile:
-
-#     csvreader = csv.reader(csvfile, delimiter=',')
-
-#     header = next(csvreader)
-  
-#     #Summary
 
 print("Financial Analysis")
 print("----------------------------")
 print(f'Total Months: {total_month}')
-print(f'Total: + {net_profit}')
+print(f'Total: {total_profit}')
 # # print("Average Change: " + )
 # # print("Greatest Increase in Profits: " + )
 # # print("Greatest Decrease in Profits: " + )
